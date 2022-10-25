3842955b55178e1f19c9f182cb3a0a8d0ab03db5
Danylo Ruzhynskyi <100782486+foxyloverGG@users.noreply.github.com>
2022-10-25 15:21:07 +0200

    creating process v1.0

--- JS/script.js
+++ JS/script.js
@@ -7,15 +7,37 @@ cardList.sort(() => Math.random() - 0.5);
 cardList = cardList.slice(0, numCards)
 cardList = cardList.concat(cardList)
 
+const cardGenerator = () => {
+    cardList.forEach((item) => {
+        const card = document.createElement('div');
+        const face = document.createElement('img');
+        const back = document.createElement('div');
+        card.classList = "card";
+        face.classList = "face";
+        back.classList = "back";
+        //Link images
+        face.src = item.image
+        //Attaching Cards to the section
+        gameboard.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+    });
+};
+
+ cardGenerator();
+
+
+
+
+
+
+
+
 // for(let card of cardList) {
 //     let div = document.createElement('div')
 //     div.innerText = card.monster
 //     gameboard.append(div)
 // }
 
-for (let card of cardList) {
-    let image = document.createElement('img')
-    image.src = card.image
-    gameboard.append(image)
-}
+
  
